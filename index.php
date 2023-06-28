b3e22bb168430397563e74ed20ecb6ff47eec138
Luskans <sylvain.migne@gmail.com>
2023-06-28 09:26:55 +0200

    add exercices p3

---
+++ index.php
@@ -0,0 +1,61 @@
+<?php
+
+/////////// EXERCICE 1 ////////////
+
+// $nombre = 0;
+// while($nombre <= 10) {
+//     echo "$nombre </br>";
+//     $nombre ++;
+// };
+
+/////////// EXERCICE 2 ////////////
+
+// $nombre1 = 0;
+// $nombre2 = 5;
+// while($nombre1 <= 20) {
+//     echo $nombre1*$nombre2."</br>";
+//     $nombre1 ++;
+// };
+
+/////////// EXERCICE 3 ////////////
+
+// $nombre1 = 100;
+// $nombre2 = 7;
+// while($nombre1 >= 10) {
+//     echo $nombre1*$nombre2."</br>";
+//     $nombre1 --;
+// };
+
+/////////// EXERCICE 4 ////////////
+
+// $nombre1 = 1;
+// while($nombre1 <= 10) {
+//     $nombre1 += ($nombre1/2);
+//     echo $nombre1."</br>";
+// };
+
+/////////// EXERCICE 5 ////////////
+
+// for ($nombre = 1; $nombre <= 15; $nombre++) {
+//     echo "On y arrive presque.<br/>";
+// };
+
+/////////// EXERCICE 6 ////////////
+
+// for ($nombre = 20; $nombre >= 0; $nombre--) {
+//     echo "C'est presque bon.<br/>";
+// };
+
+/////////// EXERCICE 7 ////////////
+
+// for ($nombre = 1; $nombre <= 100; $nombre+=15) {
+//     echo "On tient le bon bout.<br/>";
+// };
+
+/////////// EXERCICE 8 ////////////
+
+for ($nombre = 200; $nombre >= 0; $nombre-=12) {
+    echo "Enfin !!!<br/>";
+};
+
+?>
